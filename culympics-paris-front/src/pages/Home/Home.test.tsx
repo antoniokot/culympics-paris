59f80ffdd7f8745140dd55bd19cf1d3d34d23154
Antonio Hideto Borges Kotsubo <antoniokotsubo@gmail.com>
2024-05-09 22:28:40 -0300

    test(home): fix unit tests

--- culympics-paris-front/src/pages/Home/Home.test.tsx
+++ culympics-paris-front/src/pages/Home/Home.test.tsx
@@ -1,22 +1,21 @@
-import { render } from '@testing-library/react'
-import { expect, describe, it } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import { expect, describe, it, beforeEach } from 'vitest'
 
 import { Home } from './index'
 
 describe('when the page is loaded', () => {
+  // Render the component once before each test
+  beforeEach(() => {
+    render(<Home />)
+  })
+
   it('the title should be visible', () => {
-    const { getByText } = render(<Home />)
-
-    const greetings = getByText('Welcome to Culympics')
-
+    const greetings = screen.getByText('Welcome to Culympics')
     expect(greetings).not.toBeNull()
   })
 
   it('the explore button should be visible', () => {
-    const { getByText } = render(<Home />)
-
-    const greetings = getByText('EXPLORE')
-
-    expect(greetings).not.toBeNull()
+    const exploreButton = screen.getAllByText('EXPLORE')
+    expect(exploreButton[0]).not.toBeNull()
   })
 })
