5c387c3990eb6a3072ad400130bdf84cbc0c29cb
Gabriel Arruda <gabriel.arruda@ifood.com.br>
2024-06-11 21:22:50 -0300

    add fetch restaurants endpoint

--- culympics-paris-api/internal/service/restaurant.go
+++ culympics-paris-api/internal/service/restaurant.go
@@ -4,10 +4,12 @@ import (
 	"context"
 
 	"github.com/antoniokot/culympics-paris/culympics-paris-api/internal/model"
+	"github.com/google/uuid"
 )
 
 type RestaurantDao interface {
 	List(context.Context, model.RestaurantListingFilters) ([]model.Restaurant, model.Paging, error)
+	Fetch(context.Context, uuid.UUID) (model.Restaurant, error)
 }
 
 type Restaurant struct {
@@ -20,4 +22,8 @@ func NewRestaurant(dao RestaurantDao) *Restaurant {
 
 func (r Restaurant) List(ctx context.Context, filters model.RestaurantListingFilters) ([]model.Restaurant, model.Paging, error) {
 	return r.dao.List(ctx, filters)
+}
+
+func (r Restaurant) Fetch(ctx context.Context, id uuid.UUID) (model.Restaurant, error) {
+	return r.dao.Fetch(ctx, id)
 }
